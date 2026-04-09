9d4ded62fa10f3203ecdfe272f93404251f2c1e8
slothap <ueun6906@gmail.com>
2026-04-09 21:46:36 +0900

    2-4. 3-1 오류 수정

--- code/2-4.c
+++ code/2-4.c
@@ -5,12 +5,10 @@ long my_strtol(char *str, char **endptr){
     long res = 0;
 
     while(isdigit(*str)){ //이걸로도 변경 가능 *str>='0' && *str <= '9'
-        res = res * (*str - '0');
+        res = res * 10 + (*str - '0');
         str++;
     }
-    if (endptr != NULL){
-        *endptr = str;
-    }
+    *endptr = str;
     return res;
 }
 int main(){
--- code/3-1.c
+++ code/3-1.c
@@ -5,7 +5,7 @@ void read_only(const int *arr, int size)
 
     for (int i = 0; i < size; i++)
     {
-        printf("%d\n", *(arr + 1));
+        printf("%d\n", *(arr + i));
     }
 }
 int main()
